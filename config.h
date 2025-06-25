8eb689625e6f339d158dd07d2012532811475126
NecRaul <necraul@kuroneko.dev>
2025-06-25 12:42:05 +0400

    Use my wallpaper and username in config.h

--- config.h
+++ config.h
@@ -1,11 +1,11 @@
 /* user and group to drop privileges to */
-static const char *user = "nobody";	 /* replace nobody with your username */
+static const char *user = "necraul"; /* replace nobody with your username */
 static const char *group = "nobody"; /* don't touch this */
 
 static const char *colorname[NUMCOLS] = {
-	[INIT] = "black",	  /* after initialization */
-	[INPUT] = "#005577",  /* during input */
-	[FAILED] = "#CC3333", /* wrong password */
+    [INIT] = "black",     /* after initialization */
+    [INPUT] = "#005577",  /* during input */
+    [FAILED] = "#CC3333", /* wrong password */
 };
 
 /* treat a cleared input like a wrong password (color) */
@@ -13,4 +13,5 @@ static const int failonclear = 1;
 
 /* Background image path, should be available to the user above */
 /* The path should be absolute like /home/username/Pictures/Wallpapers/Wallpaper.jpg */
-static const char *background_image = "";
+static const char *background_image = "/home/necraul/Pictures/Wallpapers/Kuroneko_23.png";
+
